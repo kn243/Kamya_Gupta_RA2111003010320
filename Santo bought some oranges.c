99c5ee16bf8a4c17c4b555ff339caf422630004d
kn243 <104170651+kn243@users.noreply.github.com>
2022-04-23 15:06:15 +0530

    Add files via upload

---
+++ Santo bought some oranges.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+ int amount,billamount,q,r;
+ scanf("%d" "%d",&amount,&billamount);
+ q = amount/billamount;
+ r = amount%billamount;
+ printf("%d",q);
+ printf("\n%d",r);
+ 
+
+    return 0;
+}
